6f7ecb2581506e9c2c816e03225cbd0106db255f
Sergey <markrut23@mail.ru>
2025-05-26 23:39:49 +0400

    fix api keys

--- src/main.js
+++ src/main.js
@@ -7,14 +7,23 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { useThemeStore } from "@stores/theme";
 
+// VITE_FIREBASE_API_KEY=AIzaSyDho8uikVOpwNiQqRB1Hz8q2VALDo-M5wc
+// VITE_FIREBASE_AUTH_DOMAIN=my-vue-project-b3bc2.firebaseapp.com
+// VITE_FIREBASE_DATABASE_URL=https://my-vue-project-b3bc2-default-rtdb.europe-west1.firebasedatabase.app
+// VITE_FIREBASE_PROJECT_ID=my-vue-project-b3bc2
+// VITE_FIREBASE_STORAGE_BUCKET=my-vue-project-b3bc2.appspot.com
+// VITE_FIREBASE_MESSAGING_SENDER_ID=1078851671214
+// VITE_FIREBASE_APP_ID=1:1078851671214:web:103f519a53fc2c6791093f
+
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    apiKey: "AIzaSyDho8uikVOpwNiQqRB1Hz8q2VALDo-M5wc",
+    authDomain: "my-vue-project-b3bc2.firebaseapp.com",
+    databaseURL:
+        "https://my-vue-project-b3bc2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "my-vue-project-b3bc2",
+    storageBucket: "my-vue-project-b3bc2.appspot.com",
+    messagingSenderId: "1078851671214",
+    appId: "1:1078851671214:web:103f519a53fc2c6791093f",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
